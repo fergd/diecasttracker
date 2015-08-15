721b4d12ccb1f2bc288ad2778272be58db66133f
Christan Fergus <fergusdesignllc@gmail.com>
2015-08-15 16:43:37 -0600

    adding g fonts and did some minor tweaks

--- client/dctTest.js
+++ client/dctTest.js
@@ -1,6 +1,27 @@
 if (Meteor.isClient) {
 	Meteor.subscribe("allCars");
 	Accounts.ui.config({
-  		passwordSignupFields: "USERNAME_ONLY"
+		passwordSignupFields: "USERNAME_ONLY"
 	});
+
+	// async loader for fonts
+	// https://github.com/typekit/webfontloader
+
+	Meteor.startup(function() {
+
+	  WebFontConfig = {
+		google: { families: [ 'Montserrat::latin' ] }
+	  };
+	  (function() {
+		var wf = document.createElement('script');
+		wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		  '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+		wf.type = 'text/javascript';
+		wf.async = 'true';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(wf, s);
+		console.log("async fonts loaded", WebFontConfig);
+	  })();
+
+	})
 }
