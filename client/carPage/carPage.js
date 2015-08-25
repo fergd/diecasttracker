5d1cdbc0d74ef0fe28d938cd0d6548aa142f8898
Christan Fergus <fergusdesignllc@gmail.com>
2015-08-24 20:26:47 -0600

    added new route and js for single car display pages

--- client/carPage/carPage.js
+++ client/carPage/carPage.js
@@ -1,8 +1,11 @@
 Router.route('/car/:_id', {
-	name: 'singleCarPage',
+    name: 'singleCarPage',
     template: 'carPage',
-    data: function(){
-       var currentCar = this.params._id;
-       return CarList.findOne({ _id: currentCar });
+    data: function() {
+        var currentUserId = Meteor.userId();
+        var currentCar = this.params._id;
+        return CarList.findOne({
+            _id: currentCar
+        });
     }
 });
