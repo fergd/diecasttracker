0c38d35f56d4bb1eaa540d35e2da594027650bed
Christan Fergus <fergusdesignllc@gmail.com>
2015-08-24 21:50:16 -0600

    fiddling with this object id str thing

--- client/carPage/carPage.js
+++ client/carPage/carPage.js
@@ -1,8 +1,9 @@
-Router.route('/car/:_id', {
+Router.route('/car/:_id._str', {
     name: 'singleCarPage',
     template: 'carPage',
     data: function() {
-        var currentCar = this.params._id;
-        return CarList.findOne({_id: Meteor.Collection.ObjectID(currentCar)});
+        var currentCar = this.params._id._str;
+        return CarList.findOne({_id: currentCar});
+        //return CarList.findOne({_id: Meteor.Collection.ObjectID(currentCar)});
     }
 });
