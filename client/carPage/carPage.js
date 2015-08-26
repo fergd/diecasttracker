0eb1e393ad6732e98631fa6cc9e0f63c2cfe5e4d
Christan Fergus <fergusdesignllc@gmail.com>
2015-08-25 20:26:51 -0600

    trying to get to the object id

--- client/carPage/carPage.js
+++ client/carPage/carPage.js
@@ -1,9 +1,19 @@
+// Router.route('/car/:_id._str', {
+//     name: 'singleCarPage',
+//     template: 'carPage',
+//     data: function() {
+//         var currentCar = this.params._id._str;
+//         return CarList.findOne({_id: currentCar});
+//         //return CarList.findOne({_id: Meteor.Collection.ObjectID(currentCar)});
+//     }
+// });
 Router.route('/car/:_id._str', {
     name: 'singleCarPage',
     template: 'carPage',
     data: function() {
-        var currentCar = this.params._id._str;
+        var currentCar = ObjectID(this.params._id);
         return CarList.findOne({_id: currentCar});
-        //return CarList.findOne({_id: Meteor.Collection.ObjectID(currentCar)});
     }
 });
+
+// News.findOne({_id: new Meteor.Collection.ObjectID(this.params._id)});
