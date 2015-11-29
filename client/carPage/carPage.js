a647386af5710fd93ed2e58f3c227f1d8be1dee1
Christan Fergus <fergusdesignllc@gmail.com>
2015-11-29 16:26:18 -0700

    trying alternative ways to hide empty content

--- client/carPage/carPage.js
+++ client/carPage/carPage.js
@@ -1,18 +1,24 @@
 Router.route('/car/:_id', {
-    name: 'singleCarPage',
-    template: 'carPage',
-    data: function() {
-        var currentCar = this.params._id;
-	    return CarList.findOne({_id: currentCar});
-    }
+	name: 'singleCarPage',
+	template: 'carPage',
+	data: function() {
+		var currentCar = this.params._id;
+		return CarList.findOne({_id: currentCar});
+	},
+	onAfterAction: function () {
+		$('[data-toy-data]').each(function() {
+			if ( '' === $.trim( $(this).text() ) ) {
+		  		$(this).closest('li').remove();
+			} 
+		});
+
+		$('li').fadeOut('slow');
+	}
+
+//		$('[data-toy-data]:empty').each(function() {
+// 			console.log('before remove');
+// 			$(this).closest('.toy-info').remove();
+// 			console.log('after remove');
+// 		});
+
 });
-Template.carPage.rendered = function () {
-	//Seek out any empty data info and remove the parent list item
-	console.log('before empty func');
-	$('[data-toy-data]:empty').each(function() {
-		console.log('before remove');
-		$(this).closest('.toy-info').remove();
-		console.log('after remove');
-	});
-	console.log('after func');
-};
