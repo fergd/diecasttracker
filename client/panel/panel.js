f972102021d954c88abd22ebbc370d0067a6de63
Christan Fergus <fergusdesignllc@gmail.com>
2017-04-22 07:38:18 -0600

    added a sliding menu panel, updated the overal nav to be buttons at the bottom, various other theming layout tweaks

---
+++ client/panel/panel.js
@@ -0,0 +1,26 @@
+Template.panel.rendered = function () {
+
+    var openSidebar = function() {
+        $('.panel').addClass('is-active');
+        // $('.menu').addClass('is-active');
+        // $('#menu').addClass('toggle-close');
+        open = true;
+    }
+    var closeSidebar = function() {
+        $('.panel').removeClass('is-active');
+        // $('.menu').removeClass('is-active');
+        // $('#menu').removeClass('toggle-close');
+    }
+
+    $('.menu').click(function(event) {
+        event.stopPropagation();
+        var toggle = open ? closeSidebar : openSidebar;
+        openSidebar();
+    });
+
+    $(document).click(function(event) {
+        if (!$(event.target).closest('.panel').length) {
+            closeSidebar();
+        }
+    });
+};
