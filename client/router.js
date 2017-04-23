43072986cb47fec8e3b8b0194eb0df2a1a802953
Christan Fergus <fergusdesignllc@gmail.com>
2017-04-23 12:21:47 -0600

    adding homepage flexbox content modded panel

--- client/router.js
+++ client/router.js
@@ -13,3 +13,15 @@ Router.map(function(){
 	this.route('linkTest',{path:'/links'});
 
 });
+//SCROLL TO TOP- NEED TO IMPLEMENT JUST PUTTING HERE TEMPORARILY
+// Meteor.Router.filters({
+//     resetScroll: function(page) {
+//         if (!(page===Session.get('Router.lastPage'))) {
+//             Session.set('Router.lastPage',page);
+//             Meteor.startup(function() {window.scrollTo(0,0)});
+//         }
+//         return page;
+//     }
+// });
+
+// Meteor.Router.filter('resetScroll'); // Active for all the pages
